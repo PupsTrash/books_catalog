87a673448edcc461630747944c3e229919617800
PupsTrash <891269@mail.ru>
2021-09-26 23:46:31 +0500

    add jpaSpecificationExecutor

--- persistence/src/main/java/com/example/db/book/BookRepo.java
+++ persistence/src/main/java/com/example/db/book/BookRepo.java
@@ -1,13 +1,14 @@
 package com.example.db.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface BookRepo extends JpaRepository<BookEntity, Long> {
+public interface BookRepo extends JpaRepository<BookEntity, Long>, JpaSpecificationExecutor<BookEntity> {
 
     /**
      * Find book by id and field deleted_at is null
