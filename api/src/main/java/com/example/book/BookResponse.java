e78929040b307fd3e0709db296518ae7f12a99a5
PupsTrash <891269@mail.ru>
2021-09-26 20:03:44 +0500

    Fix api

--- api/src/main/java/com/example/book/BookResponse.java
+++ api/src/main/java/com/example/book/BookResponse.java
@@ -2,8 +2,10 @@ package com.example.book;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.Value;
 
 import java.util.List;
+import java.util.Set;
 
 @Data
 public class BookResponse {
@@ -16,7 +18,7 @@ public class BookResponse {
     private String title;
 
     @Schema(description = "Author list", example = "[\"Leo Tolstoy\"]")
-    private List<String> authors;
+    private Set<AuthorResponse> authors;
 
     @Schema(example = "2008",
             description = "Book release date")
@@ -25,5 +27,11 @@ public class BookResponse {
     @Schema(example = "978-1-85326-062-9",
             description = "The International Standard Book Number (ISBN) is a numeric commercial book identifier which is intended to be unique")
     private String isbn;
+
+    @Value
+    public static class AuthorResponse {
+        String id;
+        String name;
+    }
 
 }
